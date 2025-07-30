7b5e2e9f93f5f002d92764cb6cba03e6ca6edee9
DominicDev <s0l0estudi4r@gmail.com>
2025-07-30 11:25:43 +0200

    v0.0.1

---
+++ src/components/Background.tsx
@@ -0,0 +1,10 @@
+import '@/styles/Background.css';
+import MagicLoader from '@/components/MagicLoader';
+
+export const Background = () => {
+  return (
+    <div className='background-effect'>
+      <MagicLoader />;
+    </div>
+  );
+};
